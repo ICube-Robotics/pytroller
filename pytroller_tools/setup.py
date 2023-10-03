a93430edb429108f5efa69c6e68ff0d9e30b2a74
mcbed <mcbed.robotics@gmail.com>
2023-10-03 09:41:44 +0200

    updated cpr

--- pytroller_tools/setup.py
+++ pytroller_tools/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2023 ICube Laboratory, University of Strasbourg
+# Copyright 2023 ICube-Robotics
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- pytrollercli/pytrollercli/api/create.py
+++ pytrollercli/pytrollercli/api/create.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Open Source Robotics Foundation, Inc.
+# Copyright 2023 ICube-Robotics
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
